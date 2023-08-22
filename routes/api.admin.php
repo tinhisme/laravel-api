9c40095d4023a233bb81e2dae4be03bc04d9c485
1 <tinhnt1906@gmail.com>
2023-08-22 22:53:07 +0700

    auth

--- routes/api.admin.php
+++ routes/api.admin.php
@@ -6,5 +6,8 @@ use App\Http\Controllers\Admin\Auth\AdminLogoutController;
 use App\Http\Controllers\Admin\Auth\RefreshTokenController;
 
 Route::post('/login', [AdminLoginController::class, 'handle']);
-Route::post('/refresh-token', [RefreshTokenController::class, 'handle']);
-Route::get('/logout', [AdminLogoutController::class, 'handle']);
+
+Route::middleware(['auth:api', 'isAdmin'])->group(function() {
+    Route::post('/refresh-token', [RefreshTokenController::class, 'handle']);
+    Route::get('/logout', [AdminLogoutController::class, 'handle']);
+});
