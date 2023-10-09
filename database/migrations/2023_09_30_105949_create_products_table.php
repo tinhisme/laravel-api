78525b1af25c72ab87d07f717f00cef8f7403f67
tinhnt1906 <tinhnt1906@gmail.com>
2023-10-09 13:00:50 +0700

    feature product

--- database/migrations/2023_09_30_105949_create_products_table.php
+++ database/migrations/2023_09_30_105949_create_products_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('description')->nullable();
             $table->integer('weight')->nullable();
-            $table->integer('status')->nullable();
+            $table->integer('status')->default(1);
             $table->json('attributes')->nullable();
             $table->json('tier_variation')->nullable();
             $table->json('demension')->nullable();
